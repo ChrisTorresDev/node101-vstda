2fdd12a37e74f5c9f1b77eb401f979898a8974ac
Chris Torres <hwct83@gmail.com>
2019-12-10 14:03:35 -0800

    added mock data, two get requests

--- server/app.js
+++ server/app.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const morgan = require('morgan');
+const data = require('./data');
 
 const app = express();
 
 // add your code here
+app.use(morgan('dev'));
+
+app.get('/', function (req, res) {
+  res.status(200).send('ok');
+})
+
+app.get('/api/TodoItems', function (req, res) {
+  res.json(data).status(200);
+})
 
 module.exports = app;
